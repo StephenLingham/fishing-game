c8b356d4fa416a1038666f2a4ba6ed55515c0c5b
Stephen Lingham <147378+StephenLingham@users.noreply.github.com>
2023-11-05 14:02:16 +0000

    Fish is now caught every so many seconds

--- Assets/Scripts/FishCatching.cs
+++ Assets/Scripts/FishCatching.cs
@@ -3,8 +3,12 @@ using UnityEngine.UI;
 
 public class FishCatching : MonoBehaviour
 {
-    private float _timeToCatchFish = 5;
+    private float _timeToCatchFish = 4;
     private float _fishCatchingTimer = 0;
+
+    private float _timeToDisplayFish = 1.5f;
+    private float _displayFishTimer = 0;
+    private bool _displayingFish = false;
 
     public Transform CaughtFishImage;
     public Transform FishImages;
@@ -21,17 +25,36 @@ public class FishCatching : MonoBehaviour
             CatchFish();
         }
 
+        UpdateDisplayingFish();
+
         var displayText = $"Time till next fish: {_timeToCatchFish - _fishCatchingTimer:0.0}";
 
         DisplayText.GetComponent<Text>().text = displayText;
     }
 
+    void UpdateDisplayingFish()
+    {
+        if (!_displayingFish)
+        {
+            return;
+        }
+
+        _displayFishTimer += Time.deltaTime;
+
+        if (_displayFishTimer >= _timeToDisplayFish)
+        {
+            _displayFishTimer = 0;
+            _displayingFish = false;
+            CaughtFishImage.GetComponent<Image>().enabled = false;
+        }
+    }
+
     void CatchFish()
     {
-        // Debug.Log("Caught fish");
-
         GlobalState.NumberOfFish++;
 
         CaughtFishImage.GetComponent<Image>().overrideSprite = FishImages.GetComponent<FishImages>().Image1;
+        CaughtFishImage.GetComponent<Image>().enabled = true;
+        _displayingFish = true;
     }
 }
